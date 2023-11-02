e160a4c9eccae804ee6c4ea8ac267f3cbee58f27
laura tascon <laura.tascon@udea.edu.co>
2023-11-02 09:12:38 -0500

    modifier layout importation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import { SessionProvider } from "next-auth/react"
 import type { AppProps } from 'next/app';
 import { Provider } from 'jotai'
-import { Layout } from '@/layouts';
+import { Layout } from "@/Layouts";
+
 
 const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
   return (
