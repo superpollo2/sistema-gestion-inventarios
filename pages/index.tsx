430cddbb296bd22e1164296cc7f2175486435faf
isaacj32 <isaacj32@hotmail.com>
2023-11-08 22:15:26 -0500

    Corrección tabla

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,44 +2,46 @@ import React from "react";
 import { PrivateRoute } from "@/components/PrivateRoute";
 import Table from "@/components/Table/index";
 
-const Home = () => {
-  <Table data={[
-    {
-      "id": 123,
-      "fecha": "05/11/2023",
-      "entrada": 50,
-      "salida": 0,
-      "responsable" : "pepe el grillo"
-    }
-  ]}  columns={[
-    {
-      accessor: "id",
-      header: "Identificador"
-    },
-    {
-      accessor: "fecha",
-      header: "Fecha"
-    },
-    {
-      accessor: "entrada",
-      header: "Entrada"
-    },
-    {
-      accessor: "salida",
-      header: "Salida"
-    },
-    {
-      accessor: "responsable",
-      header: "Responsable"
-    }]}/>
-  
+const Home = () => { 
   return (
-
-    <PrivateRoute>
-      <h1>autenticated</h1>
-    </PrivateRoute>
+    <div>
+      <Table data={[
+        {
+          "id": 123,
+          "fecha": "05/11/2023",
+          "entrada": 50,
+          "salida": 0,
+          "responsable" : "pepe el grillo"
+        },
+        {
+          "id": 123,
+          "fecha": "05/11/2023",
+          "entrada": 50,
+          "salida": 0,
+          "responsable" : "pepe el grillo"
+        }
+      ]}  columns={[
+        {
+          accessor: "id",
+          header: "Identificador"
+        },
+        {
+          accessor: "fecha",
+          header: "Fecha"
+        },
+        {
+          accessor: "entrada",
+          header: "Entrada"
+        },
+        {
+          accessor: "salida",
+          header: "Salida"
+        },
+        {
+          accessor: "responsable",
+          header: "Responsable"
+        }]}/>
+        </div>
 
   );
 };
-
-export default Home;
