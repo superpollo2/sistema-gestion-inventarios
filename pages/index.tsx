d85eae6c4cd05fd662edba1b4e160beca9bb0130
isaacj32 <isaacj32@hotmail.com>
2023-11-08 19:50:42 -0500

    Front de las tablas

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,38 @@
 import React from "react";
 import { PrivateRoute } from "@/components/PrivateRoute";
+import Table from "@/components/Table/index";
 
 const Home = () => {
+  <Table data={[
+    {
+      "id": 123,
+      "fecha": "05/11/2023",
+      "entrada": 50,
+      "salida": 0,
+      "responsable" : "pepe el grillo"
+    }
+  ]}  columns={[
+    {
+      accessor: "id",
+      header: "Identificador"
+    },
+    {
+      accessor: "fecha",
+      header: "Fecha"
+    },
+    {
+      accessor: "entrada",
+      header: "Entrada"
+    },
+    {
+      accessor: "salida",
+      header: "Salida"
+    },
+    {
+      accessor: "responsable",
+      header: "Responsable"
+    }]}/>
+  
   return (
 
     <PrivateRoute>
