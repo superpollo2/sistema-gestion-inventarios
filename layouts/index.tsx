a94f6384e60917ba9d050ca48d1ae6ccde20c5d4
superpollo2 <laura.tascon@udea.edu.co>
2023-11-14 11:50:10 -0500

    Load animation component

--- layouts/index.tsx
+++ layouts/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSession } from 'next-auth/react';
 import { SideNavigationUserProfile } from '@/components/ui/Side'
-
+import { Load } from '@/components/general/Load';
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -11,12 +11,12 @@ const Layout = ({ children }: LayoutProps) => {
   const { status } = useSession();
 
   if (status === 'loading') {
-    return <h1>Loading...</h1>
+    return <Load/>
   }
 
   if (status === 'authenticated') {
     return (
-
+      
       <main className='flex'>
         <SideNavigationUserProfile />
         {children}
