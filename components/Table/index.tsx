f12a21629c926de7ee87db87c09a9d025792411e
isaacj32 <isaacj32@hotmail.com>
2023-11-16 20:20:32 -0500

    Tablas con conexion

--- components/Table/index.tsx
+++ components/Table/index.tsx
@@ -15,9 +15,15 @@ const Table = (props: Props) => {
     <div className="table-container">
       <table>
         <thead>
+<<<<<<< Updated upstream
         <tr className="border border-black w-72 bg-slate-400">
           {props.columns.map((column) => (
             <td className="border border-black w-72">
+=======
+        <tr style={{border: "1px solid rgb(0, 0, 0)"}}>
+          {columns.filter(x => !x.hidden).map((column) => (
+            <td style={{border: "1px solid rgb(0, 0, 0)", backgroundColor: "#9b9b9b", width: "300px"}}>
+>>>>>>> Stashed changes
                 { column.header }
             </td>
           ))}
@@ -27,9 +33,15 @@ const Table = (props: Props) => {
         {props.rows.map((row, index) => {
             const rowClass = index % 2 === 0 ? 'bg-gray-200' : 'bg-gray-300';
             return (
+<<<<<<< Updated upstream
               <tr className={`border border-black w-72 ${rowClass}`}>
                 {props.columns.map((column) => (
                   <td className="border border-black w-72">
+=======
+              <tr style={{border: "1px solid rgb(0, 0, 0)"}}>
+                {columns.filter(x => !x.hidden).map((column) => (
+                  <td style={{border: "1px solid rgb(0, 0, 0)"}}>
+>>>>>>> Stashed changes
                     {
                         row[column.accessor]
                     }
--- pages/inventarios.tsx
+++ pages/inventarios.tsx
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { PrivateRoute  } from "@/components/PrivateRoute";
 import Table from "@/components/Table/index";
-const inventory = () => {
-  return (
 
+const inventory = () => {
+  const [material, setMaterial] = useState("");
+  const [materials, setMaterials] = useState([]);
+  const [inventory, setInventory] = useState([]);
+  
+  useEffect(() => {
+    fetch('http://localhost:3000/api/materials')
+      .then((res) => res.json())
+      .then((data) => {
+        setMaterials(data.materials);
+        setMaterial(data.materials[0].id);
+    })
+  }, []);
+  
+  useEffect(() => {
+    fetch(`http://localhost:3000/api/Inventory?material=${material}`)
+    .then((res) => res.json())
+    .then((data) => {
+      setInventory(data.inventories);
+    });
+  }, [material]);
+  
+  return (
     <PrivateRoute >
        <div>
        <h1 className="text-3xl font-bold">Inventory Page</h1>
+<<<<<<< Updated upstream
       <Table rows={[
       {
         "id": 123,
@@ -26,6 +48,41 @@ const inventory = () => {
         header: "Full Name"
       }]}/>
     </div>
+=======
+       <select
+        value={material}
+        onChange={(e) => {
+          setMaterial(e.target.value);
+        }}
+      >
+        {materials.map((mat) => 
+        <option>{mat.id}</option>
+        )}
+      </select>
+
+      <Table data={inventory}  columns={[
+        {
+          accessor: "id",
+          header: "Identificador"
+        },
+        {
+          accessor: "movementType",
+          header: "Tipo de movimiento"
+        },
+        {
+          accessor: "quantity",
+          header: "Cantidad"
+        },
+        {
+          accessor: "materialId",
+          header: "Material"
+        },
+        {
+          accessor: "userId",
+          header: "Usuario"
+        }]}/>
+      </div>
+>>>>>>> Stashed changes
 
   
       
